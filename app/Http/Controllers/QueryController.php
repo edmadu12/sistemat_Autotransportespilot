6e3b8d962c3d3930060b03e6cf68e4fef9694e3b
Manuel Alfaro <edmadu12@gmail.com>
2023-11-16 12:12:52 -0600

    Update QueryController.php
    consulta modificada

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -7,12 +7,25 @@ use Illuminate\Http\Request;
 
 class QueryController extends Controller
 {
+    // public function getViajes()
+    // {
+    //     $viajes = DB::table('viajesreales as a')
+    //         ->select('a.cliente', 'a.origen', 'a.fecha', 'a.tracto', DB::raw('SUM(a.kilometraje) as kilometraje'))
+    //         ->whereBetween('a.fecha', ['2023-11-01 00:00:00', '2023-11-11 23:00:00'])
+    //         ->groupBy('a.cliente', 'a.origen', 'a.fecha')
+    //         ->get();
+
+    //     // Aquí puedes hacer lo que necesites con los datos de $viajes
+    //     // Por ejemplo, puedes pasarlos a la vista dashboard
+    //     return view('dashboard', ['viajes' => $viajes]);
+    // }
+
     public function getViajes()
     {
         $viajes = DB::table('viajesreales as a')
-            ->select('a.cliente', 'a.origen', 'a.fecha', 'a.tracto', DB::raw('SUM(a.kilometraje) as kilometraje'))
+            ->select('a.tracto', DB::raw('SUM(a.kilometraje) as kilometraje'))
             ->whereBetween('a.fecha', ['2023-11-01 00:00:00', '2023-11-11 23:00:00'])
-            ->groupBy('a.cliente', 'a.origen', 'a.fecha')
+            ->groupBy('a.tracto')
             ->get();
 
         // Aquí puedes hacer lo que necesites con los datos de $viajes
